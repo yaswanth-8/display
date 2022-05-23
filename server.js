15d62feb1e1c6a5dbbc8e396333fb5740b4653f1
yaswanth-8 <ya8wanth@gmail.com>
2022-05-23 22:15:04 +0530

    Firstgit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT||3000;
+
+app.set('view engine','ejs');
+
+//route
+app.get('/',(req,res)=>{
+    res.render('base',{title:"Login System"});
+})
+
+app.listen(port,()=>{
+    console.log("Listening to port");
+})
